44a0ad26a4746710da1dfd3687ecdf0595af4391
Michaelangelo Ojeani <G00416140@atu.ie>
2024-12-11 21:41:36 +0000

    react app created!

---
+++ pockettracker/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+}
+
+export default App;
